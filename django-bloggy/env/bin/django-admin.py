b2b0494db349337b61523011fbb1e93b19ed5e17
Olzhas Shaikenov <olzhas.shaikenov@gmail.com>
2014-09-30 18:58:54 -0400

    Blog Files

---
+++ django-bloggy/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/cygdrive/c/users/olzhas/desktop/realpython2/django-bloggy/env/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
